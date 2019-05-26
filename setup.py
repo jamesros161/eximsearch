09a3a8752d185dce426b0185eecf002557e8492d
James Rosado <dev@twmsllc.com>
2019-05-26 04:52:24 -0400

    test

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+
+     name='exmsearch',  
+
+     version='0.5',
+
+     scripts=['bin/exmsearch'] ,
+
+     author="James Rosado",
+
+     author_email="dev@twmsllc.com",
+
+     description= ("Exim Search Utility "),
+
+     url="https://github.com/twmsllc/eximsearch",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3.6",
+
+         "License :: GNU General Public License",
+
+     ],
+
+ )
